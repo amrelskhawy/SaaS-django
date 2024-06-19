d64e5ff673a55b7e03d1aaa4e83e49f35bfc3c31
Amr Elskhawy <amrelskhawy@gmail.com>
2024-06-19 05:25:05 +0300

    Learning Django

---
+++ src/replhome/views.py
@@ -0,0 +1,25 @@
+import json
+
+from django.core import serializers
+from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render
+from visits.models import Visit
+
+def home_page_view(request, *args, **kwargs):
+    queryset = Visit.objects.filter(path=request.path)
+    my_title = "Hello Django"
+
+    my_context = {
+        "page_title": my_title,
+        "queryset": queryset,
+    }
+
+    Visit.objects.create(
+        path=request.path
+    )
+
+    data = serializers.serialize('json', queryset)
+    return JsonResponse(data, safe=False)
+
+    # return HttpResponse(json.dumps(queryset), content_type="application/json")
+    # return render(request, template_name="home.html", context=my_context)
